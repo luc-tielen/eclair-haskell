8c10bf813ae3cd9f7052844cb40426c512ad73ca
Luc Tielen <luc.tielen@gmail.com>
2022-04-30 22:35:57 +0200

    Fix EclairSpec

--- tests/Test/Language/EclairSpec.hs
+++ tests/Test/Language/EclairSpec.hs
@@ -1,3 +1,9 @@
 module Test.Language.EclairSpec
   ( module Test.Language.EclairSpec
   ) where
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "eclair-haskell" $ parallel $ do
+  it "TODO" pending
